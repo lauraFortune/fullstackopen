25c71c80ade89d612bf26a5ccd126f62876514d7
lauraFortune <laurafordefortune@hotmail.com>
2023-07-09 23:49:42 +0100

    Exercises 4.4: Helper functions and unit tests, step2

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -2,6 +2,14 @@ const dummy = (blogs) => {
     return 1
 }
 
+const totalLikes = (blogs) => {
+
+    return blogs.length === 0
+    ? 0
+    : blogs.reduce((accum, blogItem) => accum + blogItem.likes, 0)
+}
+
 module.exports = {
-    dummy
+    dummy,
+    totalLikes
 }
