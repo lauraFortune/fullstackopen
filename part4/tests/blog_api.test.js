a7698be966235f8573a3507965882ccc5e44ed06
lauraFortune <laurafordefortune@hotmail.com>
2023-07-25 13:34:29 +0100

    Exercise 4.9: Blog list tests, step2

--- part4/tests/blog_api.test.js
+++ part4/tests/blog_api.test.js
@@ -4,14 +4,21 @@ const app = require('../app')
 const api = supertest(app)
 
 test('the correct amount of blog posts are returned in the JSON format', async () => {
-
     const response = await api
         .get('/api/blogs')
         .expect(200)
         .expect('Content-Type', /application\/json/)
-        
+    
     expect(response.body).toHaveLength(2)
 })
+
+test('blog posts have a unique identifier property named id,', async () => {
+    const response = await api.get('/api/blogs')
+    const ids = response.body.map(r => r.id)
+   
+    expect(ids).toBeDefined()
+})
+
 
 afterAll(async () => {
     await mongoose.connection.close()
